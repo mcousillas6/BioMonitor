dfc7a67f89a0a16bad4eb281127371fa40fe99d3
Mauricio Cousillas <cousillas806@gmail.com>
2017-11-05 19:15:41 -0300

    Feature/search (#8)
    * Added ph and density
    
    * Added cloudsync, updated models (#7)
    
    * Added cloudsync, updated models
    
    * Changed to HTTPotion
    
    * Added search and tags

--- web/controllers/reading_controller.ex
+++ web/controllers/reading_controller.ex
@@ -2,25 +2,13 @@ defmodule BioMonitor.ReadingController do
   use BioMonitor.Web, :controller
 
   alias BioMonitor.Routine
-  alias BioMonitor.Reading
-  @readings_per_page "30"
 
-  def index(conn, %{"routine_id" => routine_id} = params) do
+  def index(conn, %{"routine_id" => routine_id}) do
     with routine = Repo.get(Routine, routine_id),
       true <- routine != nil
     do
-      query = from r in Reading,
-        where: r.routine_id == ^routine_id
-      {readings, rummage} =
-        query |>
-        Rummage.Ecto.rummage(%{
-          "paginate" => %{
-            "per_page" => @readings_per_page,
-            "page" => "#{params["page"] || 1}"
-          }
-        })
-      readings = Repo.all(readings)
-      render(conn, "index.json", readings: readings, page_info: rummage)
+      routine = Repo.preload(routine, :readings)
+      conn |> render("index.json", %{readings: routine.readings})
     else
       false ->
         conn
--- web/controllers/routine_controller.ex
+++ web/controllers/routine_controller.ex
@@ -12,17 +12,23 @@ defmodule BioMonitor.RoutineController do
         "paginate" => %{
           "per_page" => @routines_per_page,
           "page" => "#{params["page"] || 1}"
+        },
+        "search" => %{
+          "title" => %{"assoc" => [], "search_type" => "ilike", "search_term" => params["title"]},
+          "strain" => %{"assoc" => [], "search_type" => "ilike", "search_term" => params["strain"]},
+          "medium" => %{"assoc" => [], "search_type" => "ilike", "search_term" => params["medium"]},
+          "value" => %{"assoc" => ["tags"], "search_type" => "ilike", "search_term" => params["tag"]}
         }
       })
-    routines = Repo.all(routines)
+    routines = Repo.all(routines) |> Repo.preload([:temp_ranges, :tags])
     render(conn, "index.json", routine: routines, page_info: rummage)
-    end
+  end
 
   def create(conn, %{"routine" => routine_params}) do
     changeset = Routine.changeset(%Routine{}, routine_params)
     case Repo.insert(changeset) do
       {:ok, routine} ->
-        Endpoint.broadcast("sync", "new_routine", Map.put(routine_params, :uuid, routine.uuid))
+        routine = routine |> Repo.preload([:temp_ranges, :tags])
         conn
         |> put_status(:created)
         |> put_resp_header("location", routine_path(conn, :show, routine))
@@ -35,16 +41,15 @@ defmodule BioMonitor.RoutineController do
   end
 
   def show(conn, %{"id" => id}) do
-    routine = Repo.get!(Routine, id)
+    routine = Repo.get!(Routine, id) |> Repo.preload([:temp_ranges, :tags])
     render(conn, "show.json", routine: routine)
   end
 
-  def update(conn, %{"id" => id, "routine" => routine_params} = params) do
-    routine = Repo.get!(Routine, id)
+  def update(conn, %{"id" => id, "routine" => routine_params}) do
+    routine = Repo.get!(Routine, id) |> Repo.preload([:temp_ranges, :tags])
     changeset = Routine.changeset(routine, routine_params)
     case Repo.update(changeset) do
       {:ok, routine} ->
-        Endpoint.broadcast("sync", "update_routine", Map.put(params, :uuid, routine.uuid))
         render(conn, "show.json", routine: routine)
       {:error, changeset} ->
         conn
@@ -56,7 +61,6 @@ defmodule BioMonitor.RoutineController do
   def delete(conn, %{"id" => id}) do
     routine = Repo.get!(Routine, id)
     Repo.delete!(routine)
-    Endpoint.broadcast("sync", "delete_routine", %{uuid: routine.uuid})
     send_resp(conn, :no_content, "")
   end
 
@@ -69,5 +73,46 @@ defmodule BioMonitor.RoutineController do
     routine = Repo.get!(Routine, id)
     Endpoint.broadcast("sync", "start", %{id: routine.id})
     render(conn, "show.json", routine: routine)
+  end
+
+  def sync_update(conn, %{"routine_id" => routine_uuid, "routine" => routine_params}) do
+    with routine = Repo.get_by(Routine, uuid: routine_uuid),
+      true <- routine != nil,
+      changeset = Routine.changeset(routine, routine_params),
+      {:ok, _routine} <- Repo.update(changeset)
+    do
+      conn
+      |> put_status(200)
+      |> render(conn, "show.json", routine: routine)
+    else
+      false ->
+        changeset = Routine.changeset(%Routine{}, routine_params)
+        case Repo.insert(changeset) do
+          {:ok, routine} ->
+            conn
+            |> put_status(200)
+            |> render(conn, "show.json", routine: routine)
+          {:error, changeset} ->
+            conn
+            |> put_status(422)
+            |> render(BioMonitor.ChangesetView, "error.json", changeset: changeset)
+        end
+      {:error, changeset} ->
+        conn
+        |> put_status(422)
+        |> render(BioMonitor.ChangesetView, "error.json", changeset: changeset)
+    end
+  end
+
+  def sync_delete(conn, %{"routine_id" => routine_uuid}) do
+    with routine = Repo.get_by!(Routine, uuid: routine_uuid),
+      true <- routine != nil,
+      {:ok, _struct} <- Repo.delete(routine)
+    do
+      send_resp(conn, :no_content, "")
+    else
+      _ ->
+        send_resp(conn, :unprocessable_entity, "")
+    end
   end
 end
