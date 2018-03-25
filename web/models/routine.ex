312a903fe19751b6896aca9346340ea502397350
mcousillas6 <cousillas806@gmail.com>
2018-03-25 13:15:06 -0300

    Fixed child relationships

--- web/models/routine.ex
+++ web/models/routine.ex
@@ -24,9 +24,9 @@ defmodule BioMonitor.Routine do
     field :trigger_after, :integer
     field :trigger_for, :integer
     has_many :readings, BioMonitor.Reading, on_delete: :delete_all
-    has_many :temp_ranges, BioMonitor.TempRange, on_delete: :delete_all
-    has_many :log_entries, BioMonitor.LogEntry, on_delete: :delete_all
-    has_many :tags, BioMonitor.Tag, on_delete: :delete_all
+    has_many :log_entries, BioMonitor.LogEntry, on_delete: :delete_all, on_replace: :delete
+    has_many :temp_ranges, BioMonitor.TempRange, on_delete: :delete_all, on_replace: :delete
+    has_many :tags, BioMonitor.Tag, on_delete: :delete_all, on_replace: :delete
 
     timestamps()
   end
